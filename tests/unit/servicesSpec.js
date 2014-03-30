8b998769b735bcba402d0f2e1acf8f6cac402ef7
Jesse Webb <gweebz@gmail.com>
2014-03-30 01:44:56 -0600

    Refactor use generic data

--- tests/unit/servicesSpec.js
+++ tests/unit/servicesSpec.js
@@ -14,8 +14,8 @@ describe('services', function() {
         describe('getBindableKeysAsync()', function() {
             it('should request bindable keys data and return promise', function() {
                 var expectedBindableKeys = [
-                    { "name": "+ (Plus)", "bind": "kp_plus" },
-                    { "name": "Enter", "bind": "kp_enter" }
+                    { "name": "Bindable Key 1", "bind": "bk_1" },
+                    { "name": "Bindable Key 2", "bind": "bk_2" }
                 ];
                 $httpBackend.expectGET('data/bindable-keys.json').respond(expectedBindableKeys);
 
@@ -33,9 +33,9 @@ describe('services', function() {
         describe('getPrimaryWeaponsAsync()', function() {
             it('should request primary weapons data and return promise', function() {
                 var expectedPrimaryWeapons = [
-                    { "name": "SG 553 / AUG", "bind": "sg556,aug" },
-                    { "name": "AWP", "bind": "awp" },
-                    { "name": "G3SG1 / SCAR-20", "bind": "g3sg1,scar20" }
+                    { "name": "Primary Weapon 1", "bind": "pw1,pw1alt" },
+                    { "name": "Primary Weapon 2", "bind": "pw2" },
+                    { "name": "Primary Weapon 3", "bind": "pweap3,pw3b" }
                 ];
                 $httpBackend.expectGET('data/primary-weapons.json').respond(expectedPrimaryWeapons);
 
@@ -53,8 +53,8 @@ describe('services', function() {
         describe('getSecondaryWeaponsAsync()', function() {
             it('should request secondary weapons data and return promise', function() {
                 var expectedSecondaryWeapons = [
-                    { "name": "Glock-18 / P2000 / USP-S", "bind": "glock,hkp2000" },
-                    { "name": "Dual Berettas", "bind": "elite" }
+                    { "name": "Secondary Weapon 1", "bind": "sweap1,sweap1b" },
+                    { "name": "Secondary Weapon 2", "bind": "sw2" }
                 ];
                 $httpBackend.expectGET('data/secondary-weapons.json').respond(expectedSecondaryWeapons);
 
@@ -72,8 +72,8 @@ describe('services', function() {
         describe('getGearAsync()', function() {
             it('should request gear data and return promise', function() {
                 var expectedGear = [
-                    { "name": "Kevlar Vest", "bind": "vest" },
-                    { "name": "Kevlar + Helmet", "bind": "vesthelm" }
+                    { "name": "Gear 1", "bind": "gear" },
+                    { "name": "Gear Item 2", "bind": "gear2" }
                 ];
                 $httpBackend.expectGET('data/gear.json').respond(expectedGear);
 
@@ -91,8 +91,8 @@ describe('services', function() {
         describe('getGrenadesAsync()', function() {
             it('should request grenades data and return promise', function() {
                 var expectedGrenades = [
-                    { "name": "Molotov / Incendiary", "bind": "molotov,incgrenade" },
-                    { "name": "Decoy", "bind": "decoy" }
+                    { "name": "Grenade #1", "bind": "nade1" },
+                    { "name": "Grenade 2", "bind": "grenade2" }
                 ];
                 $httpBackend.expectGET('data/grenades.json').respond(expectedGrenades);
 
