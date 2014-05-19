f71061863196b03a812c6cda6d161f370eda336e
Jesse Webb <gweebz@gmail.com>
2014-05-18 18:58:49 -0600

    Improve <title> content on each page

--- js/app.js
+++ js/app.js
@@ -17,11 +17,19 @@ buyndsApp.config(['ngClipProvider', function(ngClipProvider) {
 buyndsApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         controller: 'SingleKeyGenCtrl',
-        templateUrl: 'partials/single-key-generator.phtml'
+        templateUrl: 'partials/single-key-generator.phtml',
+        title: 'Single-Key Generator'
     });
     $routeProvider.when('/mkg', {
         controller: 'MultiKeyGenCtrl',
-        templateUrl: 'partials/multi-key-generator.phtml'
+        templateUrl: 'partials/multi-key-generator.phtml',
+        title: 'Multi-Key Generator'
     });
     $routeProvider.otherwise({redirectTo: '/'});
+}]);
+
+buyndsApp.run(['$rootScope', '$route', function ($rootScope, $route) {
+    $rootScope.$on("$routeChangeSuccess", function(){
+        $rootScope.title = $route.current.title;
+    });
 }]);
