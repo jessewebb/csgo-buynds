b2905ea718a1d9774a1f47bff7ab9ddbe520f604
Jesse Webb <gweebz@gmail.com>
2015-07-19 13:42:33 -0600

    Add Travis CI build configuration to run Javascript tests using npm, Karma, & Jasmine

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Karma configuration
+// http://karma-runner.github.io/0.13/config/configuration-file.html
+
+module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files: [
+            'vendor/jquery-2.1.3/jquery.min.js',
+            'vendor/angular-1.3.12/angular.min.js',
+            'vendor/angular-1.3.12/angular-mocks.js',
+            'js/*.js',
+            'tests/specs/*.spec.js'
+        ],
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['Chrome', 'Firefox'],
+        singleRun: false
+    })
+};
